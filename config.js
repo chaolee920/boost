3691430a30f03d9c78b9b38b3a80c35d95168e7d
Bilal Hjiouaj <bilal@sternsoftware.com>
2020-09-18 14:44:22 -0500

    initial project setup

---
+++ config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+	JWT_SECRET: process.env.JWT_SECRET || 'boost',
+	DB_URI: process.env.DB_URI || 'mongodb://andriy:andriy@ds151702.mlab.com:51702/lesson',
+	MISSING_PARAMETER: 'MISSING_PARAMETER',
+	DB_ERROR: 'DB_ERROR',
+	EMAIL_DUPLICATION: 'EMAIL_DUPLICATION',
+	AUTHENTICATION_FAILED: 'AUTHENTICATION_FAILED',
+	NOT_FOUND: 'NOT_FOUND',
+};
