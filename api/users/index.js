2aa56dc7c579d79a133ed76a1662db5d40069dd0
Bilal Hjiouaj <bilal@sternsoftware.com>
2020-10-06 10:53:30 -0500

    get current logged in user

--- api/users/index.js
+++ api/users/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const controller = require('./user.controller');
 const router = express.Router();
+const authService = require('../auth.service');
 
 router.post('/', controller.signup);
 router.post('/login', controller.login);
+router.get('/me', authService.checkAuthentication, controller.getMe);
 
 module.exports = router;
